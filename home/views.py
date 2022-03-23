2c1e41256a95d25d427079c00048190040822f07
EwanColquhoun <ewancolquhoun@hotmail.com>
2022-03-23 09:48:11 +0000

    Add about page and css. Update home urls and views.

--- home/views.py
+++ home/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index (request):
+def index(request):
     """A view to return index page."""
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """A view to return the About page."""
+    return render(request, 'home/about.html')
