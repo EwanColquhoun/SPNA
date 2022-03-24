fddd0474a6862689c13d4b21ba5640fc742f426f
EwanColquhoun <ewancolquhoun@hotmail.com>
2022-03-24 22:10:17 +0000

    Basic js for modal

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,16 @@ $('#contact-button').click('shown.bs.modal', function() {
     console.log('button press')
 })
 
+let button = document.getElementById('contact-button')
+let modal = document.getElementById('myModal')
+
+button.addEventListener('click', function() {
+    modal.classList.add('show')
+    modal.style.display = 'block'
+    console.log('show')
+})
+    
+modal.addEventListener('shown.bs.modal', function () {
+    button.focus()
+    console.log('focus')
+  })
