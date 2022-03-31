79c1b4c060e050e90a9b8ecada97fa7e2e7b25ab
EwanColquhoun <ewancolquhoun@hotmail.com>
2022-03-31 17:06:43 +0000

    Added customclearfilewidget

--- news/forms.py
+++ news/forms.py
@@ -13,15 +13,15 @@ class ArticleForm(forms.ModelForm):
         Article form for spna_admin
         """
         model = Articles
-        fields = ('title', 'content', 'image', 'campaign')
+        fields = ('title', 'content', 'image',)
         widgets = {
             'content': SummernoteWidget(),
+            'image': CustomClearableFileInput()
         }
-        image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+        # image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['title'].label = 'Article Title'
         self.fields['content'].label = 'Article Content'
-        self.fields['campaign'].label = 'Campaign'
