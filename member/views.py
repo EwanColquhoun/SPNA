aedd4add56fe2e2c852bab5dc83a01c5a6dce20c
EwanColquhoun <ewancolquhoun@hotmail.com>
2022-04-02 10:00:56 +0000

    Add Documents to memeber area initial.

--- member/views.py
+++ member/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import Document
 
 # Create your views here.
 def member_area(request):
     """ 
     A view to return the members area
     """
+    docs = Document.objects.all()
 
-    return render(request, 'member/member-area.html')
+    context = {
+        'docs': docs,
+    }
+    return render(request, 'member/member-area.html', context)
