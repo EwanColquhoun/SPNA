1dedc29eb93cdc44627706e848daecdd0d86460a
EwanColquhoun <ewancolquhoun@hotmail.com>
2022-04-05 09:39:53 +0000

    Add js for the select all boxes to autopopulate the email_to field

--- spna_admin/static/spna_admin/js/spna_admin.js
+++ spna_admin/static/spna_admin/js/spna_admin.js
@@ -5,12 +5,20 @@ window.addEventListener('DOMContentLoaded', function() {
     console.log('ready')
 })
 
+let msa = document.getElementById('members-select-all')
+msa.addEventListener('change', checkAllMembers)
 
+let csa = document.getElementById('contacts-select-all')
+csa.addEventListener('change', checkAllContacts)
+
+
+// Variables
+let emailField = document.querySelector('#id_email_to');
+let emailList = [];
+let selectButtons = document.querySelectorAll('.select-box');
+
+// Activated the checkbox to autopopulate email_to field
 function selectButton() {
-    let selectButtons = document.querySelectorAll('.select-box')
-    let emailField = document.querySelector('#id_email_to')
-    let emailList = []
-
     selectButtons.forEach((person) => {
         person.addEventListener('change', function () {
             if (person.checked) {
@@ -26,40 +34,52 @@ function selectButton() {
 
 // select all from: https://stackoverflow.com/questions/7251005/javascript-select-all-checkboxes-in-a-table
 
-let msa = document.getElementById('members-select-all')
-msa.addEventListener('change', checkAllMembers)
-
-let csa = document.getElementById('contacts-select-all')
-csa.addEventListener('change', checkAllContacts)
-
+// Select box for all members
 function checkAllMembers() {
     var cbs = document.querySelectorAll('.member-select-box');
+    // let emailField = document.querySelector('#id_email_to')
+    // let emailList = []
 
     for(var i=0; i < cbs.length; i++) {
 
         if(cbs[i].type == 'checkbox') {
             cbs[i].checked = this.checked;
+             if (cbs[i].checked) {
+                let addy = cbs[i].getAttribute('value')
+                console.log(addy)
+                emailList.push(addy);
+            } else {
+                emailList.pop(-1)
+            }
         };
+        emailField.value = emailList
     };
 };
 
+// Selectbox for all contacts
 function checkAllContacts() {
     var cbs = document.querySelectorAll('.contact-select-box');
 
     for(var i=0; i < cbs.length; i++) {
         if(cbs[i].type == 'checkbox') {
         cbs[i].checked = this.checked;
+         if (cbs[i].checked) {
+                let addy = cbs[i].getAttribute('value')
+                console.log(addy)
+                emailList.push(addy);
+            } else {
+                emailList.pop(-1)
+            }
+        };
+        emailField.value = emailList
         }
-    }
-}
-
+};
 
 // Bootstrap tooltips
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
-
 
 // Gets the delete modal working
 const modal_buttons = document.getElementById('delete-cont-modal-buttons');
