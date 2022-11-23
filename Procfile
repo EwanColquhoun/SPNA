b217dc52b183ecded7f3f387cc64ba81223d0fdd
EwanColquhoun <ewancolquhoun@hotmail.com>
2022-11-23 23:01:11 +0000

    Prepare for render deployment

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn spna.wsgi:application
--- spna/settings.py
+++ spna/settings.py
@@ -27,6 +27,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['localhost', 'scottishpna.herokuapp.com']
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 CSRF_TRUSTED_ORIGINS = [
     "https://localhost",
     "https://scottishpna.herokuapp.com"]
